2fb88e43680af8d01d33a03659db6731248965eb
Gabriel Femi <gabrielfemi799@gmail.com>
2020-09-25 04:10:59 +0100

    Correct directory manipulation

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -10,13 +10,13 @@ class ContactServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                '../config/contact.php' => config_path('contact.php'),
+                __DIR__ . '/../config/contact.php' => config_path('contact.php'),
             ]);
 
             $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
             $this->loadViewsFrom(__DIR__ . '/views','contact');
             $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-            $this->mergeConfigFrom( '../config/contact.php', 'contact');
+            $this->mergeConfigFrom( __DIR__ . '/../config/contact.php', 'contact');
         }
 
     }
