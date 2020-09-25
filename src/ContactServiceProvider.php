af12b1d1e67f2d562bc9aca344c5bbc8e0bd594b
Gabriel Femi <gabrielfemi799@gmail.com>
2020-09-25 02:08:03 +0100

    Add validation in service provider

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -8,13 +8,17 @@ class ContactServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
             $this->publishes([
-             '../config/contact.php' => config_path('contact.php'),
-        ]);
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views','contact');
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->mergeConfigFrom( '../config/contact.php', 'contact');
+                '../config/contact.php' => config_path('contact.php'),
+            ]);
+
+            $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+            $this->loadViewsFrom(__DIR__ . '/views','contact');
+            $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+            $this->mergeConfigFrom( '../config/contact.php', 'contact');
+        }
+
     }
 
     public function register()
